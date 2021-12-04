c85ca42832f7a8e6175bea54bcefcfc9d726ada6
alehandrodol <alexeydol2002@mail.ru>
2021-12-04 19:39:59 +0300

    Минорный комит

--- core/database.py
+++ core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi import Request
 
-SQLALCHEMY_DATABASE_URL = "postgresql://alexey:Tozafa_alex02@localhost/kursach"
+SQLALCHEMY_DATABASE_URL = ""
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
